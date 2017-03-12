655da81ed89c897b2f3b0ef512fcdfbaec6bd6fc
matias <luraschi8@gmail.com>
2017-03-12 15:08:24 -0300

    startup de la api

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by matias on 3/12/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
