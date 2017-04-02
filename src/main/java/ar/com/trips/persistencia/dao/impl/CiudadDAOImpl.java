dd5c18f53ab89eb24de6a88aeddd35cbe53d11b7
franery <franery10@gmail.com>
2017-04-02 15:47:35 -0300

    Sigo probando

--- src/main/java/ar/com/trips/persistencia/dao/impl/CiudadDAOImpl.java
+++ src/main/java/ar/com/trips/persistencia/dao/impl/CiudadDAOImpl.java
@@ -3,6 +3,8 @@ package ar.com.trips.persistencia.dao.impl;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import ar.com.trips.persistencia.dao.ICiudadDAO;
@@ -10,9 +12,12 @@ import ar.com.trips.persistencia.modelo.CiudadModelo;
 
 public class CiudadDAOImpl extends DAOImpl implements ICiudadDAO {
 
+	@Autowired
+	protected SessionFactory sessionFactoryAux;
+	
 	@Override
 	public boolean ciudadExistente(CiudadModelo ciudadModelo) {
-		Session session = sessionFactory.openSession();
+		Session session = sessionFactoryAux.openSession();
 		String query = "FROM " + CiudadModelo.class.getName() + " c WHERE c.nombre = '" + ciudadModelo.getNombre() + "'"
 						+ " AND c.pais = '" + ciudadModelo.getPais() + "'";
 		@SuppressWarnings("unchecked")
@@ -23,7 +28,7 @@ public class CiudadDAOImpl extends DAOImpl implements ICiudadDAO {
 	
 	@Override
 	public List<CiudadModelo> listar() {
-		Session session = sessionFactory.openSession();
+		Session session = sessionFactoryAux.openSession();
 		@SuppressWarnings("unchecked")
 		List<CiudadModelo> lista = (List<CiudadModelo>)session.createQuery("from "  + CiudadModelo.class.getName() + " where borrado = 0").list();
 		session.close();
@@ -32,7 +37,7 @@ public class CiudadDAOImpl extends DAOImpl implements ICiudadDAO {
 	
 	@Transactional
 	public void borrar(long id) {
-		Session s = sessionFactory.openSession();
+		Session s = sessionFactoryAux.openSession();
 		s.beginTransaction();
 		CiudadModelo model = s.get(CiudadModelo.class, id);
 		model.setBorrado(1);
