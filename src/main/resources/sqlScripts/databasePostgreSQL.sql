e6e2d68fb5ee1d3e3c593fce152b5e22ae089f76
franery <franery10@gmail.com>
2017-05-14 15:05:32 -0300

    Cambio a postgresssql

--- src/main/resources/sqlScripts/databasePostgreSQL.sql
+++ src/main/resources/sqlScripts/databasePostgreSQL.sql
@@ -34,19 +34,25 @@ create table Atraccion (
 	id serial primary key not null,
 	nombre varchar(50) not null,
     idCiudad bigint not null,
-    horario varchar(100),
-    descripcion varchar(250),
-    precio varchar(100),
     latitud float not null,
     longitud float not null,
     borrado int default 0 not null,
     plano bytea,
-    audioEN bytea,
-    audioES bytea,
     video bytea,
     recorrible int default 0,
-    idioma varchar(2),
     foreign key (idCiudad) references Ciudad (id)
+);
+
+create table AtraccionIdioma (
+	id serial primary key not null,
+    idAtraccion bigint not null,
+	idioma varchar(3) not null,
+    horario varchar(100),
+    descripcion varchar(250),
+    borrado tinyint default 0 not null,
+    precio varchar(100),
+    audio bytea,
+    foreign key (idAtraccion) references Atraccion (id)
 );
 
 create table ImagenAtraccion (
@@ -59,15 +65,21 @@ create table ImagenAtraccion (
 create table PuntoDeInteres (
 	id serial primary key not null,
     nombre varchar(50),
-    descripcion varchar(250),
-    orden int,
     borrado int default 0 not null,
     imagen bytea,
-    audioEN bytea,
-    audioES bytea,
     idAtraccion bigint,
-    idioma varchar(2),
     foreign key (idAtraccion) references Atraccion (id)
+);
+
+create table PuntoIdioma (
+	id serial primary key not null,
+    idioma varchar(3) not null,
+    descripcion varchar(250),
+    orden tinyint,
+    borrado tinyint default 0 not null,
+    audio bytea,
+    idPunto bigint,
+    foreign key (idPunto) references PuntoDeInteres (id)
 );
 
 create table Resenia (
